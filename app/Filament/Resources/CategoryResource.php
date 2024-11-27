18c0ace5325abd93c085875144443060ae935e6c
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-11-27 21:09:58 +0100

    auto slug generation
    To generate a slug after providing a title we need to use afterStateUpdated, and provide some parameters: string operation (create, edit), string state (current data from TextInput), Forms\Set set (we can also provide get (get a data from another TextInput and Category category (name of model and variable in lowercase, we can use it to use for example fields from model)). Note: it will not work on create page

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -16,6 +16,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class CategoryResource extends Resource
 {
@@ -29,7 +30,14 @@ class CategoryResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')->required(),
+                TextInput::make('name')->required()
+                ->live(onBlur: true)
+                ->afterStateUpdated(function (string $operation, string $state, Forms\Set $set){
+                    if ($operation === 'edit'){
+                        return;
+                    }
+                    $set('slug', Str::slug($state));
+                }),
                 TextInput::make('slug')->required(),
             ]);
     }
