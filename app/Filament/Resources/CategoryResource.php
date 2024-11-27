028d66a6de76ff1ccd0414e79a94e93668ed0f7d
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-11-27 18:44:20 +0100

    authorization
    we can use policy's in filament to check that it is the admin or, for example, the editor who is use method. Note: don't forget to add function to check admin status for user. Also we can use $shouldSkipAuthorization when we want to skip authorization for chosen resource

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -25,6 +25,8 @@ class CategoryResource extends Resource
 
     protected static ?string $modelLabel = 'Post Categories';
 
+//    protected static bool $shouldSkipAuthorization = true;  use when we want to skip authorization for chosen resource
+
     public static function form(Form $form): Form
     {
         return $form
